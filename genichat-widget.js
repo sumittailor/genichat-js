5c08996f6bae7084c4c0c5dd52e364fa6ebcaaf2
sumittailor <mygenius1717@gmail.com>
2025-12-16 17:10:19 +0530

    Refactor GeniChat widget initialization and functionality

--- genichat-widget.js
+++ genichat-widget.js
@@ -235,7 +235,8 @@
     msg.includes("activate") ||
     msg.includes("activation") ||
     msg.includes("device activate") ||
-    msg.includes("start device")
+    msg.includes("start device") ||
+    msg.includes("Active / Live")
 ) {
     return "🔗 <b>Support Page:</b> <a href='https://fetaca.com/support/' target='_blank'>https://fetaca.com/support/</a><br><br>" +
 
@@ -281,6 +282,7 @@
     }
 
 })();
+
 
 
 
